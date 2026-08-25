e8b573ca2e06bce78c7adf69abdc0f0da09bc2f0
pedrojano <pedrino022@gmail.com>
2026-08-25 10:34:37 -0300

    fix: correção de cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,16 @@
+import express from 'express';
+import cors from 'cors';
 import { app } from './app';
 import { env } from './config/env';
 import { startExpireOrderJob } from './jobs/expireOrder.Job';
+
+app.use(cors({
+    origin: [
+        'https://site-feminnita.vercel.app',
+        'http://localhost:3000'
+    ],
+    credentials: true
+}));
 
 app.listen(env.port, () => {
     console.log(`Server is Runing on Port ${env.port}`)
