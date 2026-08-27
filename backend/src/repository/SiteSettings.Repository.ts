850172726a28e8a231b39afc26b05453e72c878d
pedrojano <pedrino022@gmail.com>
2026-08-27 16:42:16 -0300

    feat: adicionada a função de cotação de frete

--- backend/src/repository/SiteSettings.Repository.ts
+++ backend/src/repository/SiteSettings.Repository.ts
@@ -1,6 +1,13 @@
+import { eq } from 'drizzle-orm';
 import { db } from '../config/db';
 import { siteSettings } from '../db/schema';
 
 export function findAll() {
     return db.query.siteSettings.findMany();
+}
+
+export function findByKey(key: string) {
+    return db.query.siteSettings.findFirst({
+        where: eq(siteSettings.key, key),
+    });
 }
